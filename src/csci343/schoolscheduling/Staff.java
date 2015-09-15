3d3693ac76709bcf789b9264126a935a835141aa
Amanda Lail <alail@unca.edu>
2015-09-15 19:31:20 -0400

    staff

--- src/csci343/schoolscheduling/Staff.java
+++ src/csci343/schoolscheduling/Staff.java
@@ -5,6 +5,8 @@
  */
 
 package csci343.schoolscheduling;
+import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -13,14 +15,23 @@ package csci343.schoolscheduling;
 public class Staff {
     
     //private String positions[]={"Professor", "Associate Profesor", "Assistant Professor", "Lecturer", "Adjuct Professor", "Administrative Assistant", "Staff"};
-    private int staff;
+    private int salary;
     private int month;
     private int day;
     private int year;
     
-   // public static Staff random(){
+   public static Staff random(){
+       Staff temp = new Staff();
+        Random rand = new Random();
+        temp.salary = rand.nextInt(71)*1000+30000;
+        temp.month = rand.nextInt(12)+1;
+        temp.day = rand.nextInt(28)+1;
+        temp.year = rand.nextInt(5)+2012;
         
-   // }
-    
+        return temp;
+    }
+    public String toString(){
+        return salary + ", " + month + ", " + day + ", " + year;
+    }
     
 }
