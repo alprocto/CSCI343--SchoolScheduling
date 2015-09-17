b780be1e45e4f497d297982ca08d462dc60d177f
Drew Proctor <alprocto@gmail.com>
2015-09-17 10:16:34 -0400

    Merge origin/master
    Conflicts:
    	src/csci343/schoolscheduling/Subject.java

--- src/csci343/schoolscheduling/Subject.java
+++ src/csci343/schoolscheduling/Subject.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -62,48 +61,7 @@ public class Subject {
 //           generateAddress
     
     public int getID(){
-        
+        return 1;
     }
     
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package csci343.schoolscheduling;
-
-import java.util.*;
-
-/**
- *
- * @author alprocto
- */
-public class Subject {
-    protected static String categories[];
-    protected String category;
-    protected String subjectName;
-    protected String description;
-    
-    /**
-     *
-     * @param category
-     * @param subjectName
-     * @param description
-     */
-    public Subject(String category, String subjectName, String description){
-        this.category=category;
-        this.subjectName=subjectName;
-        this.description=description;
-    }
-    
-    @Override
-    public String toString(){
-        return category + "\t" + subjectName + "\t" + description;
-    }
-           
-//           generateAddress
-    
-  
 }
