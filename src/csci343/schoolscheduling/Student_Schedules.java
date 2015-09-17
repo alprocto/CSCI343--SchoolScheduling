829ddbdb604d64a589358e14eccfab7cfb9e606f
Amanda Lail <alail@unca.edu>
2015-09-17 11:39:35 -0400

    txt file

---
+++ src/csci343/schoolscheduling/Student_Schedules.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package csci343.schoolscheduling;
+
+/**
+ *
+ * @author Amanda
+ */
+public class Student_Schedules {
+    
+}
